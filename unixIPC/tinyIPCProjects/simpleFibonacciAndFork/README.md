83946ee3c686ea9a67fb86b13f1d965d8101c7bd
Siqueira <rodrigosiqueiramelo@gmail.com>
2017-08-18 11:23:39 -0300

    Basic IPC program using shared memory and fork

---
+++ unixIPC/tinyIPCProjects/simpleFibonacciAndFork/README.md
@@ -0,0 +1,22 @@
+# Small fibonacci application with Shared Memory and Fork
+
+----
+## About 
+
+> This is a silly application wherein a shared memory is created, than a child
+process compute the fibonacci number and write it into the shared memory.
+Finally, parent processes print the sequence and made the clean up.
+
+## Compile
+
+```
+make
+```
+
+## Execute
+
+> 
+
+```
+bin/main <number>
+```
--- unixIPC/tinyIPCProjects/simpleFibonacciAndFork/src/main.c
+++ unixIPC/tinyIPCProjects/simpleFibonacciAndFork/src/main.c
@@ -5,13 +5,23 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/wait.h>
+#include <sys/shm.h>
 
 #include "fibonacci.h"
+
+#define MAX_SEQUENCE 10
+
+typedef struct
+{
+  long fib_sequence[MAX_SEQUENCE];
+  int sequence_size;
+} shared_data;
 
 int main(int argc, char ** argv)
 {
   pid_t childProcess;
 
+  // Validate parameters
   if (argc != 2)
   {
     printf("Invalid number of arguments\n");
@@ -20,7 +30,46 @@ int main(int argc, char ** argv)
   }
 
   int targetFibonnaci = atoi(argv[1]);
+  if (targetFibonnaci > MAX_SEQUENCE)
+  {
+    printf("Sadly we cannot support %d number\n", targetFibonnaci);
+    printf("Try to recompile with a larger number of MAX_SEQUENCE\n");
+    exit(1);
+  }
+  else if (targetFibonnaci < 2)
+  {
+    printf("Provide an integer number greater than 1\n");
+    exit(1);
+  }
 
+  // Shared memory
+  shared_data * pointerToSharedData = 0;
+  key_t key = ftok("./", 'I');
+
+  if (key < 0)
+  {
+    perror("ftok");
+    exit(1);
+  }
+
+  int sharedMemoryID = shmget(key, sizeof(shared_data), 0644 | IPC_CREAT);
+  if (sharedMemoryID < 0)
+  {
+    perror("shmget");
+    exit(1);
+  }
+
+  pointerToSharedData = (shared_data *)shmat(sharedMemoryID, (void *)0, 0);
+  if ((char *)(pointerToSharedData) == (char *)(-1))
+  {
+    perror("shmat");
+    shmctl(sharedMemoryID, IPC_RMID, NULL);
+    exit(1);
+  }
+
+  pointerToSharedData->sequence_size = targetFibonnaci;
+
+  // Child and parent
   childProcess = fork();
   if (childProcess < 0)
   {
@@ -31,18 +80,37 @@ int main(int argc, char ** argv)
   // Child
   if (!childProcess)
   {
-    for (int i = 1; i <= targetFibonnaci; i++)
+    for (int i = 0, f = 1; f <= pointerToSharedData->sequence_size; i++, f++)
     {
-      printf(" %d ", fibonacci(i));
+      pointerToSharedData->fib_sequence[i] = fibonacci(f);
     }
-    printf ("\n");
     exit(0);
   }
   // Parent
   else
   {
     wait(NULL);
-    printf("Parent process say: bye bye\n");
+    for (int i = 0; i <= pointerToSharedData->sequence_size; i++)
+    {
+      printf ("%ld ", pointerToSharedData->fib_sequence[i]);
+    }
+    printf("\n");
+  }
+
+  // Clean up stuffs
+  printf("Parent processes clean up and say Bye Bye!\n");
+  int status = shmdt((void *)pointerToSharedData);
+  if (status < 0)
+  {
+    perror("shmdt");
+    exit(1);
+  }
+
+  status = shmctl(sharedMemoryID, IPC_RMID, NULL);
+  if (status < 0)
+  {
+    perror("shmctl");
+    exit(1);
   }
 
   return 0;
